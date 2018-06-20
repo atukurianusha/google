6c9571fc540d157eea7d6445cf674ae90a9e8493
baldoorravikumar <38450572+baldoorravikumar@users.noreply.github.com>
2018-06-20 11:43:59 +0530

    added some info

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!
+                           
+                           hai ravi welcome to DevOps
+                           and AWS as well" );
     }
 }
