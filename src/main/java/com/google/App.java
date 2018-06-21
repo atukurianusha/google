163f5eb678d6fbc488fe49b89be2ce1b011eeac0
baldoorravikumar <38450572+baldoorravikumar@users.noreply.github.com>
2018-06-21 06:45:35 +0530

    Update App.java

--- src/main/java/com/google/App.java
+++ src/main/java/com/google/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!
-                           
+                           hai to all
                            hai ravi welcome to DevOps
                            and AWS as well" );
     }
